e6b1cd177953db7655783c504295f66bdece57b3
ExoDestroy <58120913+ExoDestroy@users.noreply.github.com>
2021-05-20 11:17:07 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,31 +1,36 @@
 // Select color input
-let colorInput = document.body.querySelector('h2');
-let colorToChoose = colorInput[1].querySelector('#colorPicker');
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+var color;
 
- // Select size input
-const sizeForm = document.querySelector('form');
-let sizeHeight = sizeForm.querySelector('#inputHeight');
-let sizeWidth = sizeForm.querySelector('#inputWidth');
-let submit = sizeForm.querySelector("submit");
+function makeGrid(x, y) {
+  'use strict';
+  var n, t;
+  for (n = 1; n <= x; n += 1) {
+    $('#pixelCanvas').append('<tr id=table' + n + '></tr>');
+    for (t = 1; t <= y; t += 1) {
+      $('#table' + n).append('<td></td>');
+    }
+  }
+  $('tr').remove();
+  // Add color to cells
+  $('td').click(function addColor() {
+    color = $('#colorPicker').val();
+
+    if ($(this).attr('style')) {
+      $(this).removeAttr('style');
+    } else {
+      $(this).attr('style', 'background-color:' + color);
+    }
+  });
+
+}
 
 // When size is submitted by the user, call makeGrid()
-submit.addEventListener('click', function(evt) {
-    evt.classList.toggle('myTable');
-    makeGrid();
+$('#sizePicker').submit(function(event) {
+  'use strict';
+  event.preventDefault();
+  var width = $('#inputWeight').val(),
+    height = $('#inputHeight').val();
+  makeGrid(height, width);
 });
-
-function makeGrid() {
-
-// Your code goes here!
-    const getTableGrid = document.body.querySelector('h2');
-    let tableGrid = getTableGrid[2].querySelector('#pixelCanvas');
-    for (let r = sizeWidth; r <= sizeWidth; r--) {
-        for (let c = sizeHeight; c <= sizeHeight; c--) {
-            const grid = tableGird[r][c];
-            grid[r][c].addEventListener('click', function () {
-                grid[r][c] = colorToChoose;
-            });
-        }
-    }
-
-};
