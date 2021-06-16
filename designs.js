9cd7ab5073667988a63a5dbbac2263434a8173b5
ExoDestroy <58120913+ExoDestroy@users.noreply.github.com>
2021-06-15 21:40:16 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,36 +1,29 @@
-// Select color input
-/*jslint browser: true*/
-/*global $, jQuery, alert*/
-var color;
-
-function makeGrid(x, y) {
-  'use strict';
-  var n, t;
-  for (n = 1; n <= x; n += 1) {
-    $('#pixelCanvas').append('<tr id=table' + n + '></tr>');
-    for (t = 1; t <= y; t += 1) {
-      $('#table' + n).append('<td></td>');
-    }
-  }
-  $('tr').remove();
-  // Add color to cells
-  $('td').click(function addColor() {
-    color = $('#colorPicker').val();
-
-    if ($(this).attr('style')) {
-      $(this).removeAttr('style');
-    } else {
-      $(this).attr('style', 'background-color:' + color);
-    }
-  });
-
-}
+// Select color and size inputs
+const colorPicker = document.getElementById('colorPicker');
+const columns = document.getElementById('inputHeight');
+const rows = document.getElementById('inputWidth');
+const canvas = document.getElementById('pixelCanvas');
+const form = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function(event) {
-  'use strict';
-  event.preventDefault();
-  var width = $('#inputWeight').val(),
-    height = $('#inputHeight').val();
-  makeGrid(height, width);
+form.addEventListener('submit', function(x) {
+  pixelCanvas.innerHTML = '';
+  x.preventDefault();
+  makeGrid();
 });
+
+pixelCanvas.addEventListener('click', function(y) {
+  if (y.target.nodeName === 'TD') {
+    y.target.style.backgroundColor = colorPicker.value;    
+  }
+})
+
+function makeGrid() {
+  for (let i = 0; i < rows.value; i++) {
+    const row = pixelCanvas.insertRow(0);
+    for (let o = 0; o < columns.value; o++) {
+      row.insertCell(0);
+    }
+  }
+
+}
