0b73a92517eaa59a7514244ef4125f6008801064
ExoDestroy <58120913+ExoDestroy@users.noreply.github.com>
2021-06-25 20:41:59 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -12,13 +12,17 @@ form.addEventListener('submit', function(x) {
   makeGrid();
 });
 
+// When user clicks on a specific box, fills in the user's selected color
 pixelCanvas.addEventListener('click', function(y) {
   if (y.target.nodeName === 'TD') {
+    // Line of code that fills in the color of the selected box
     y.target.style.backgroundColor = colorPicker.value;    
   }
 })
 
+// The function responsible for forming the grid
 function makeGrid() {
+  // Nested loop for making a x by y grid
   for (let i = 0; i < rows.value; i++) {
     const row = pixelCanvas.insertRow(0);
     for (let o = 0; o < columns.value; o++) {
